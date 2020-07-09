c273fe6b2bc978397aa7ad2d21110e829cdcea0a
Ramesh <ramesh.ramasamy@gyanmatrix.com>
2020-07-09 20:21:32 +0530

    skipping pkg_resources get version

--- oauth2_provider/__init__.py
+++ oauth2_provider/__init__.py
@@ -1,6 +1,6 @@
-import pkg_resources
+#import pkg_resources
 
 
-__version__ = pkg_resources.require("django-oauth-toolkit")[0].version
+__version__ = "0.12.1"  # pkg_resources.require("django-oauth-toolkit")[0].version
 
 default_app_config = "oauth2_provider.apps.DOTConfig"
